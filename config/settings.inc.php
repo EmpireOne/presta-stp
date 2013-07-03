4f53b0ad47197713e314c981fc431a22d9f12962
EmpireOne Group DEV <empireone_d@treutlen.dreamhost.com>
2013-07-03 00:00:27 -0700

    adding config file changes

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,8 +1,8 @@
 <?php
-define('_DB_SERVER_', '');
-define('_DB_NAME_', '');
-define('_DB_USER_', '');
-define('_DB_PASSWD_', '');
+define('_DB_SERVER_', 'eopdb1.empireone.com.au');
+define('_DB_NAME_', 'sydneytruckparts_com_au_dev');
+define('_DB_USER_', 'empireone_d');
+define('_DB_PASSWD_', 'YNn2DaFW');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
@@ -10,9 +10,9 @@ define('_PS_CACHE_ENABLED_', '0');
 define('_MEDIA_SERVER_1_', '');
 define('_MEDIA_SERVER_2_', '');
 define('_MEDIA_SERVER_3_', '');
-define('_COOKIE_KEY_', '');
-define('_COOKIE_IV_', '');
+define('_COOKIE_KEY_', 'v9U1mAocQw0CzH7k6R5fdjy0xm5sLI6Kvta31CfQCZut9g6mJQA8UWkE');
+define('_COOKIE_IV_', 'vCPrxYhz');
 define('_PS_CREATION_DATE_', '2013-06-20');
 define('_PS_VERSION_', '1.5.4.1');
-define('_RIJNDAEL_KEY_', '');
-define('_RIJNDAEL_IV_', '');
+define('_RIJNDAEL_KEY_', 'DeM5DEahomPmENW5HvTz7uAGcJsM5fm9');
+define('_RIJNDAEL_IV_', 'gK3GCIqb9sr3nnokCd8zbw==');
